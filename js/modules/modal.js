1d392c96f7a1b199015d9cc8ea84625b00020d7f
Ed William <edwilliamcardoso@gmail.com.br>
2025-11-11 12:03:48 -0300

    modal refatorado com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,24 +1,45 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
-
-  function toggleModal(event) {
-    event.preventDefault();
-    containerModal.classList.toggle("ativo");
+export default class Modal {
+  constructor (botaoAbrir, botaoFechar, containerMOdal) {
+  this.botaoAbrir = document.querySelector(botaoAbrir);
+  this.botaoFechar = document.querySelector(botaoFechar);
+  this.containerModal = document.querySelector(containerMOdal);
+  this.eventToggleModal = this.eventToggleModal.bind(this);
+   this.cliqueForaModal = this.cliqueForaModal.bind(this);
   }
-  function cliqueForaModal(event) {
-    if (event.target === this) {
-      toggleModal(event);
+ 
+ toggleModal() {
+    event.preventDefault();
+    this.containerModal.classList.toggle("ativo");
+  }
+
+eventToggleModal(event){
+event,preventDefault();
+this.toggleModal();
+}
+
+
+ cliqueForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal();
     }
   }
 
-  if (botaoAbrir && botaoFechar && containerModal) {
-    botaoAbrir.addEventListener("click", toggleModal);
-    botaoFechar.addEventListener("click", toggleModal);
-    containerModal.addEventListener("click", cliqueForaModal);
-  }
+
+
+addModalEvent() {
+  this.botaoAbrir.addEventListener("click", this.eventToggleModal);
+  this.botaoFechar.addEventListener("click", this.eventToggleModal);
+  this.containerModal.addEventListener("click", this.cliqueForaModal);
 }
+
+init() {
+ if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+  this.addModalEvent();
+ }
+ return this;
+}
+
+
 
 // fetch("/some-html-page")
 //   .then((response) => response.text())
