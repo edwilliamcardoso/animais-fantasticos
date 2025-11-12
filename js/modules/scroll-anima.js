a38fdcda20627f02233083322f0980eabca2c4b5
Ed William <edwilliamcardoso@gmail.com.br>
2025-11-12 14:54:10 -0300

    animacao ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -2,23 +2,37 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-    this.animaScroll = this.animaScroll.bind(this).
+    this.checkDistance = this.checkDistance.bind(this):
   }
+getDistance(){
+  this.distance = [...this.sections].map((section) => {
+     const offset = section.offset5Top;
+     return {
+      rçrmrnt : section,
+      offset: Math.floor.windowMetade - this.windowMetade
+     };
+  });
+}
+checkDistance() {
+  
+}
+
 
   // eslint-disable-next-line no-inner-declarations
   animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
+      this.sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
       const isSectionVisible = sectionTop - this.windowMetade < 0;
-      if (isSectionVisible) section.classList.add("ativo");
-      else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
-      }
     });
+    
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener("scroll", this.animaScroll);
+      if (this.sections.length) {
+    this.getDistance();
+    this.checkDistance();
+    window.addEventListener("scroll", this.checkDistance);
   }
+  return this;
+}
 }
